e51ca953569ff9a2e63e3cfab6c6812b8fa3fec7
Frédéric <61278179+Fred-si@users.noreply.github.com>
2023-10-07 14:24:29 +0200

    Add Black

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,10 @@
 def main() -> str:
     return hello_world()
 
+
 def hello_world() -> str:
     return f"{hello()}, {world()}!".capitalize()
+
 
 def hello() -> str:
     """Say hello.
@@ -11,6 +13,7 @@ def hello() -> str:
     'hello'
     """
     return "hello"
+
 
 def world() -> str:
     """Say world.
--- src/main/main_test.py
+++ src/main/main_test.py
@@ -1,5 +1,6 @@
 from .main import hello_world
 
+
 class MainTest:
     def test_main_should_return_hello_world(self) -> None:
         assert hello_world() == "Hello, world!"
